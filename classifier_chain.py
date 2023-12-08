3d414aee0a4b463fbd60590db0303aa6d61ede3b
Yiran Fan <yiranf@umich.edu>
2023-12-07 18:51:17 -0800

    update classifier chain

--- classifier_chain.py
+++ classifier_chain.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.multioutput import ClassifierChain
-from sklearn.metrics import jaccard_score
+from sklearn import metrics
 
 
 stem = np.loadtxt('feature_matrix_stem.txt')
@@ -22,7 +22,7 @@ base_lr = LogisticRegression()
 ovr = OneVsRestClassifier(base_lr)
 ovr.fit(X_train, y_train)
 Y_pred_ovr = ovr.predict(X_test)
-ovr_jaccard_score = jaccard_score(y_test, Y_pred_ovr, average="samples")
+ovr_jaccard_score = metrics.f1_score(y_test, Y_pred_ovr, average="samples")
 
 # Fit an ensemble of logistic regression classifier chains and take the
 # take the average prediction of all the chains.
@@ -32,12 +32,12 @@ for chain in chains:
 
 Y_pred_chains = np.array([chain.predict(X_test) for chain in chains])
 chain_jaccard_scores = [
-    jaccard_score(y_test, Y_pred_chain >= 0.5, average="samples")
+    metrics.f1_score(y_test, Y_pred_chain >= 0.5, average="samples")
     for Y_pred_chain in Y_pred_chains
 ]
 
 Y_pred_ensemble = Y_pred_chains.mean(axis=0)
-ensemble_jaccard_score = jaccard_score(
+ensemble_jaccard_score = metrics.f1_score(
     y_test, Y_pred_ensemble >= 0.5, average="samples"
 )
 
@@ -71,7 +71,7 @@ ax.grid(True)
 ax.set_title("Classifier Chain Ensemble Performance Comparison")
 ax.set_xticks(x_pos)
 ax.set_xticklabels(model_names, rotation="vertical")
-ax.set_ylabel("Jaccard Similarity Score")
+ax.set_ylabel("F1 Score")
 ax.set_ylim([min(model_scores) * 0.9, max(model_scores) * 1.1])
 colors = ["r"] + ["b"] * len(chain_jaccard_scores) + ["g"]
 ax.bar(x_pos, model_scores, alpha=0.5, color=colors)
